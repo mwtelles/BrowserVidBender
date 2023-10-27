3b8efeeb507b6121fa2ee612bd27deb34768c52e
Matheus Telles <business.mwtelles@gmail.com>
2023-10-26 23:37:26 -0300

    consumindo o worker

--- app/pages/file-upload/src/app.js
+++ app/pages/file-upload/src/app.js
@@ -3,22 +3,35 @@ import View from "./views.js";
 const view = new View();
 const clock = new Clock();
 
+const worker = new Worker('./src/worker/worker.js', { type: 'module' });
+
+worker.onerror = (error) => {
+    console.log('error worker', error);
+}
+
+worker.onmessage = ({ data }) => {
+    if (data.status !== 'done') return;
+    clock.stop();
+    view.updateElapsedTime(`O processo levou ${took.replace("atrás", "")}`);
+}
+
 let took = "";
 
 view.configureOnFileChange((file) => {
+  const canvas = view.getCanvas();
+  worker.postMessage({
+    file,
+    canvas
+  }, [canvas]);
+
   clock.start((time) => {
     took = time;
     view.updateElapsedTime(`Processo iniciado há ${time}`);
   });
-
-  setTimeout(() => {
-    clock.stop();
-    view.updateElapsedTime(`O processo levou ${took.replace("atrás", "")}`);
-  }, 5000);
 });
 
 async function fakeFetch() {
-    const filePath = 'videos/frag_bunny.mp4';
+    const filePath = '/videos/frag_bunny.mp4';
     const response = await fetch(filePath);
     const file = new File([await response.blob()], filePath, { type: 'video/mp4', lastModified: Date.now() });
     const event = new Event('change');
